f998f389daba636ed56963ae2cc74e082021ebcf
Mario <62727998+Mariosignorelli@users.noreply.github.com>
2022-02-16 15:56:19 -0300

    firdt commit

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+namespace Csharp1
+{
+    class Program
+    {
+        static void Main( String[] args)
+        {
+            int numerosDeVezes = 10;
+            for (int i = 0; i < numerosDeVezes; i++)
+            {
+                Console.WriteLine($"Bem vindo ao curso de .NET {i}");
+            }
+        }
+    }
+}
+
